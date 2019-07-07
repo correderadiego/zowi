3453b5cc44cddbce88c354af4dec57a10806f175
correderaDiego <correderaDiego@gmail.com>
2019-07-07 13:00:23 +0200

    Se incluyen los ficheros de configuración para Sloeber.

--- sloeber.ino.cpp
+++ sloeber.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2019-07-06 09:53:25
+//This file has been generated on 2019-07-07 12:59:41
 
 #include "Arduino.h"
 #include <Arduino.h>
